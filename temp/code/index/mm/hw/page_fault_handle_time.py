cf42c9cd306c1d706c14363841c090671ad9c746
hdsFFFFFFFF <hdsFFFFFF@163.com>
2020-08-07 19:33:03 +0800

    add new code

--- temp/code/index/mm/hw/page_fault_handle_time.py
+++ temp/code/index/mm/hw/page_fault_handle_time.py
@@ -1,35 +1,55 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from __future__ import print_function
 from bcc import BPF
 
 b = BPF(text = '''
         #include <uapi/linux/ptrace.h>
-        /*
-         * 如果使用高精度定时器，为了防止溢出，需要使用一个更大的变量类型来存储
-         * ktime_t是一个以nanosecond精度来表示的墙上时间
-         */
         #include <linux/ktime.h>
+
+        BPF_HASH(timer, u32, ktime_t);
 
         int kprobe__handle_mm_fault(struct pt_regs *ctx)
         {
+                u32 pid = bpf_get_current_pid_tgid();
                 ktime_t start = bpf_ktime_get_ns();
+                
+                timer.update(&pid, &start);
 
-                bpf_trace_printk("start:%lld\\n", start);
-        
                 return 0;
         }
 
-        int kretprobe__handle_mm_fault(struct pt_regs *ctx, ktime_t *k)
+        int kretprobe__handle_mm_fault(struct pt_regs *ctx)
         {
-                int ret = PT_REGS_RC(ctx);
-
                 ktime_t end = bpf_ktime_get_ns();
+                int ret = PT_REGS_RC(ctx);
+                
+                u32 pid = bpf_get_current_pid_tgid();
+                
+                ktime_t delta;
+                ktime_t *tsp = timer.lookup(&pid);
+                if ((ret >= 0) && (tsp != NULL))
+                        delta = end - *tsp;
 
-                bpf_trace_printk("end time:%lld\\n", end);
+                bpf_trace_printk("%lld\\n", delta);
 
                 return 0;
         }
         ''')
 
-b.trace_print(fmt = '{5, 6}')
+print("Tracing for page fault handling time...Ctrl-C to end")
+
+while True:
+    try:
+        (tsk, pid, cpu, flags, ts, us) = b.trace_fields()#内核时间以纳秒(ns)为单位
+        #1s = 1000毫秒(ms)        
+        #1ms = 1000微妙(us)
+        #1us = 1000纳秒(ns)
+        #1ns = 1000皮秒(ps)
+        ms = float(us) / 1000000    
+        print('handle time:%8.4fms\n' % (ms))
+    except KeyboardInterrupt:
+        exit()
+        
+
